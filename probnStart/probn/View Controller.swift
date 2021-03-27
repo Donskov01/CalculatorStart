0cc9762fe1586caeb4ad342cc7bfd97939b0f968
RomirazzGroup <74821822+RomirazzGroup@users.noreply.github.com>
2021-03-27 22:29:57 +0300

    Add files via upload

---
+++ probnStart/probn/View Controller.swift
@@ -0,0 +1,25 @@
+//
+//  View Controller.swift
+//  probn
+//
+//  Created by Family on 14.03.2021.
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+      
+    
+    @IBOutlet weak var result: UILabel!
+    
+    @IBAction func didgits(_ sender: Any) {
+  
+    }
+    @IBAction func buttons(_ sender: Any) {
+        
+        
+    }
+    
+}
